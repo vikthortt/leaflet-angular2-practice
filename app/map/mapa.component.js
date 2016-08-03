65541a7927905e84b6cd5757d3d9d697dab3e41b
Victor Torres <vikthort.t@gmail.com>
2016-08-03 11:34:16 -0500

    Make map fullscreen

--- app/map/mapa.component.js
+++ app/map/mapa.component.js
@@ -7,7 +7,11 @@
     template: `
       <style>
         .map {
-          height: 500px;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
         }
       </style>
       <div class="map" id="mymap"></div>
